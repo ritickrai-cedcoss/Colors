5671107f493390b83ad9b63e45463b6fd2a8966e
ritick <ritick.official@gmail.com>
2022-02-11 20:44:38 +0530

    commiting the color pallete

---
+++ src/script.js
@@ -0,0 +1,20 @@
+
+function colorone(){
+    document.getElementById("bodycolor").style.background="#FE657D";
+}
+
+function colortwo(){
+    document.getElementById("bodycolor").style.background="#FCB2A9";
+}
+
+function colorthree(){
+    document.getElementById("bodycolor").style.background="#E3E3DB";
+}
+
+function colorfour(){
+    document.getElementById("bodycolor").style.background="#94E4C9";
+}
+
+function colorfive(){
+    document.getElementById("bodycolor").style.background="#509B7D";
+}
